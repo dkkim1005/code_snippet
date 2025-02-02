43c82b5d7b32817f76074341abe438663196c129
dkkim1005 <dkkim1005@gmail.com>
2025-02-03 02:59:53 +0900

    removed redundant files

--- vec/target/debug/deps/vec-c246671234ff9dfe.d
+++
@@ -1,5 +0,0 @@
-/Users/francis-kim/Work/code_snippet/vec/target/debug/deps/vec-c246671234ff9dfe: src/main.rs
-
-/Users/francis-kim/Work/code_snippet/vec/target/debug/deps/vec-c246671234ff9dfe.d: src/main.rs
-
-src/main.rs:
--- vec/target/debug/vec.d
+++
@@ -1,1 +0,0 @@
-/Users/francis-kim/Work/code_snippet/vec/target/debug/vec: /Users/francis-kim/Work/code_snippet/vec/src/main.rs
